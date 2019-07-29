c1e21bb962f8b8ad6239c902afdacdc5a1ae2efa
essjay05 <stepjoyous@gmail.com>
2019-07-29 14:12:02 -0700

    Created team schema/model.

---
+++ models/team.js
@@ -0,0 +1,11 @@
+// REQUIRE MONGOOSE
+const mongoose = require('mongoose');
+
+// Create TEAM schema
+const teamSchema = new mongoose.Schema({
+    name: String,
+    members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Superhero'}]
+}, { timestamps: true });
+
+// Make exportable... creating "name to call" and template/schema for team
+module.exports = mongoose.model('Team', teamSchema);
