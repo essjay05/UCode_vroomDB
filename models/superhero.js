9477c9e5d5750fcf6beb7599644e512777d55930
essjay05 <stepjoyous@gmail.com>
2019-07-29 14:03:18 -0700

    Created superhero schema/model.

--- models/superhero.js
+++ models/superhero.js
@@ -1,1 +1,22 @@
 // Require mongoose
+const mongoose = require('mongoose');
+
+// Create Quote Schema
+const quoteSchema = new mongoose.Schema({
+    line: String
+}, { timestamps: true });
+
+// Create Superhero Schema
+const superheroSchema = new mongoose.Schema({
+    heroName: { type: String, required: true},
+    alias: { type: String, required: true},
+    placeOfBirth: { type: String},
+    team: { type: mongoose.Schema.Types.ObjectId, ref: 'Team'},
+    quotes: [quoteSchema]
+}, { timestamps: true });
+
+// Make this a Model
+const Superhero = mongoose.model('Superhero', superheroSchema);
+
+// Make exportable
+module.exports = Superhero;
